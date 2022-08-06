480e73a62feedf04f20dd3d4fad9d4c7e8741d43
Rush <kriscookies20@gmail.com>
2022-08-06 16:53:54 -0700

    created the thought type definition and resolver

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,8 +2,24 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-type Query {
-    thoughts: # what goes here?
+type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  type Query {
+    thoughts(username: String): [Thought]
   }
 `;
 
