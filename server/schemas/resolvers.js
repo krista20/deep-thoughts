f08f0d5efe85fd168c7d34239e5fc728c16d844b
Rush <kriscookies20@gmail.com>
2022-08-08 14:53:01 -0700

    added in auth

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { User, Thought } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
@@ -27,8 +28,9 @@ const resolvers = {
   Mutation: {
     addUser: async (parent, args) => {
         const user = await User.create(args);
+        const token = signToken(user);
       
-        return user;
+        return { token, user };
       },
       login: async (parent, { email, password }) => {
         const user = await User.findOne({ email });
@@ -43,7 +45,8 @@ const resolvers = {
           throw new AuthenticationError('Incorrect credentials');
         }
       
-        return user;
+        const token = signToken(user);
+        return { token, user };
       }
   }
 };
